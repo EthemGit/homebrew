4a3d7add28cbcca061eef3c74aa803d219f2209a
Ethem <73109035+EthemGit@users.noreply.github.com>
2022-12-11 17:43:39 +0100

    added first package as formatted string

--- analytics.py
+++ analytics.py
@@ -1,1 +1,11 @@
-print('Test')
+import json
+import requests
+
+r = requests.get('https://formulae.brew.sh/api/formula.json')
+packages_json = r.json()  # list of packages
+first_package = packages_json[0]
+
+package_url = 'https://formulae.brew.sh/api/formula/a2ps.json'
+
+packages_str_first_package = json.dumps(first_package, indent=2)
+print(packages_str_first_package)
